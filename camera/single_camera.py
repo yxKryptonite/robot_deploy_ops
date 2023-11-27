0c2595e1b5a1afbcec843a262e274471c41750e5
Kryptonite <1535820963@qq.com>
2023-11-27 14:34:08 -0500

    test camera

--- camera/single_camera.py
+++ camera/single_camera.py
@@ -110,6 +110,10 @@ if __name__ == "__main__":
     c, d = camera.get_image_with_depth()
     print(c.shape, d.shape)
     c, d = camera.get_image_with_depth() # take a second shot
+    for i in d:
+        for j in i:
+            if j > 10 and j < 1000:
+                print(j)
     os.makedirs("./color",exist_ok=True)
     os.makedirs("./depth",exist_ok=True)
     cv2.imwrite("./color/1.png", c)
